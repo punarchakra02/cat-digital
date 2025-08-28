a33a4b45da0287e890ab70bf0f9e20eae0a43b0c
Girendra Singh <126884309+Giru-03@users.noreply.github.com>
2025-08-28 12:51:10 +0530

    new

--- catrent/catrent/settings.py
+++ catrent/catrent/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-lys^j7=u&oe-xe@5r#=7(ig!$buak6&vtkdj_m%41tb*)ldym9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
